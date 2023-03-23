bcae6d87b53e3f32078085b46adaa411af62afe3
irdof <62650179+irdof@users.noreply.github.com>
2023-03-23 19:00:48 +0100

    Update external-shape.py

--- external-shape.py
+++ external-shape.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 # Constantes
-R = 5.3
+R = 4
 n = 2
 m = 1.92
 L_AV = 6.8
@@ -14,13 +14,30 @@ X_M = 3
 # functions
 def f_AV(x,y) :
     r = np.sqrt((x-X_M)**2+y**2)
-    if x <= L_AV + X_M and X_M <= x :
-        return R * (1-((r)/L_AV)**m)**(1/m)
-    else :
-        return np.nan
+    return R * (1-((r)/L_AV)**m)**(1/m)
     
 def g_AV(x,y) :
     return -f_AV(x,y)
+
+def f_M(x,y):    
+    return R
+    
+def g_M(x,y):
+    return -f_M(x,y)
+
+def f(x,y) :
+    if abs(y) <= 2*R :
+        if (x <= L_AV + X_M and X_M <= x ):
+            return f_AV(x,y)
+        elif x < X_M and x >= 0 and abs(y) <= R:
+            return f_M(x,y)
+        else :
+            return np.nan
+    else :
+        return np.nan
+def g(x,y) :
+    return -f(x,y)
+    
 
 def compute_grid(X,Y,Z,func):
     i = 0
@@ -33,29 +50,34 @@ def compute_grid(X,Y,Z,func):
         i = i + 1
         
 # Compute data
-N = 300
-x_AV = np.linspace(X_M,X_M+L_AV,N)
-y_AV = np.linspace(0,L_AV,N)
+N = 200
+x = np.linspace(-L_AR,X_M+L_AV,N)
+y= np.linspace(-2*R,2*R,N)
 
-X, Y = np.meshgrid(x_AV, y_AV)
+X, Y = np.meshgrid(x, y)
 Y_neg = -Y
-Z1 = np.zeros(X.shape)
-Z2 = np.zeros(X.shape)
-Z3 = np.zeros(X.shape)
-Z4 = np.zeros(X.shape)
 
-compute_grid(X,Y,Z1,f_AV)
-compute_grid(X,Y,Z2,g_AV)
-compute_grid(X,Y_neg,Z3,f_AV)
-compute_grid(X,Y_neg,Z4,g_AV)
+# AV
+Z_1 = np.zeros(X.shape)
+Z_2 = np.zeros(X.shape)
+Z_3 = np.zeros(X.shape)
+Z_4 = np.zeros(X.shape)
 
-#Plot
+compute_grid(X,Y,Z_1,f)
+compute_grid(X,Y,Z_2,g)
+# compute_grid(X,Y_neg,Z_3,f)
+# compute_grid(X,Y_neg,Z_4,g)
+
+
+
+#Plot 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
-ax.contour3D(X, Y, Z1, 50, cmap='binary')
-ax.contour3D(X, Y, Z2, 50, cmap='binary')
-ax.contour3D(X, Y_neg, Z3, 50, cmap='binary')
-ax.contour3D(X, Y_neg, Z4, 50, cmap='binary')
+# plot AV
+ax.contour3D(X, Y, Z_1, 50, cmap='binary')
+ax.contour3D(X, Y, Z_2, 50, cmap='binary')
+# ax.contour3D(X, Y_neg, Z_3, 50, cmap='binary')
+# ax.contour3D(X, Y_neg, Z_4, 50, cmap='binary')
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z');
