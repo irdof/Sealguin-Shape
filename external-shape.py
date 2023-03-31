249bcfdcecad0eadd4a33717db29e16ab01d2d94
irdof <62650179+irdof@users.noreply.github.com>
2023-03-31 07:48:12 +0200

    Update external-shape.py

--- external-shape.py
+++ external-shape.py
@@ -1,117 +1,139 @@
+# from mpl_toolkits import mplot3d
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+
+# # Constantes
+# R = 4
+# n = 2
+# m = 1.92
+# L_AV = 6.8
+# L_AR = 5
+# X_M = 3
+
+
+
+# def f_AV(x) :
+#     try :
+#         l = np.log(R) + 1/m * np.log(1 - ((x - X_M)/L_AV)**m)
+#         return np.exp(l)
+#     except RuntimeWarning:
+#         print("hello")
+
+# def f(x) :
+#     if (x <= L_AV + X_M and X_M <= x ):
+#         return f_AV(x)
+#     elif x < X_M and x >= 0 :
+#         # return f_M(x,y)
+#         return 1
+#     else :
+#         return 1
+
+
+
+# N = 200
+# phi = np.linspace(-np.pi, np.pi, N)
+# z = np.linspace(X_M,X_M+L_AV,N)
+# Z = np.array(N * [z])
+# X  = np.zeros(Z.shape)
+# Y = np.zeros(Z.shape)
+# r = np.zeros(Z.shape)
+
+# for i in range(N) :
+#     for j in range(N) :
+#         r[i,j] = f(Z[i,j])
+
+# X = r*np.cos(phi)
+# Y = r*np.sin(phi)
+# Y = Y.T
+
+# #Plot 
+# fig = plt.figure()
+# ax = plt.axes(projection='3d')
+# # plot AV
+# ax.plot_surface(X, Y, Z, cmap='binary')
+
+# # ax.contour3D(X, Y_neg, Z_3, 50, cmap='binary')
+# # ax.contour3D(X, Y_neg, Z_4, 50, cmap='binary')
+# ax.set_xlabel('x')
+# ax.set_ylabel('y')
+# ax.set_zlabel('z');
+# plt.show()
+# print("debug")
+
+import matplotlib
 from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
 
+matplotlib.use('TkAgg',force=True)
+print("Switched to:",matplotlib.get_backend())
 
 # Constantes
 R = 4
 n = 2
 m = 1.92
-L_AV = 6.8
+L_AV = 3
 L_AR = 5
 X_M = 3
+N = 500
 
-# functions
+def pol2cart(theta,r,z):
+    return [np.cos(theta)*r,np.sin(theta)*r,z]
 
-def f_AV_amp(x) :
-    r = np.sqrt((x-X_M)**2)
-    return R * (1-((r)/L_AV)**m)**(1/m)
+def f_av(Z) :
+    return R*(1-((Z[0]-X_M)/L_AV)**m)**(1/m)
 
-def f_AV(x,y) :
-    Theta = np.arcsin(y/R)
-    return f_AV_amp(x)*np.cos(Theta)
+def f_m(Z) :
+    return R * np.ones(Z.shape)
 
+def plotShape(t,r,z):
+    [x,y,z] = pol2cart(t,r,z)
+    #Plot 
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    # plot AV
+    ax.plot_surface(x,y,z, cmap='binary')
 
-def g_AV(x,y) :
-    return -f_AV(x,y)
-
-def f_M_amp(x):
-    return R
-
-def f_M(x,y):    
-    Theta = np.arcsin(y/R)
-    return R*np.cos(Theta)
-    
-def g_M(x,y):
-    return -f_M(x,y)
-
-def f(x,y) :
-    if abs(y) <= 2*R :
-        if (x <= L_AV + X_M and X_M <= x ):
-            return f_AV(x,y)
-        elif x < X_M and x >= 0 and abs(y) <= R:
-            return f_M(x,y)
-        else :
-            return np.nan
-    else :
-        return np.nan
-
-def f_amp(x):
-    if (x <= L_AV + X_M and X_M <= x ):
-        return f_AV_amp(x)
-    elif x < X_M and x >= 0 :
-        return f_M_amp(x)
-    else :
-        return np.nan
-
-def g(x,y) :
-    return -f(x,y)
-    
-
-def compute_grid(X,Y,Z,func,func_amp):
-    i = 0
-    j = 0
-    # for coorZip in zip (X,Y) :
-    #     lim = func_amp(X[0,i])
-    #     Y[:,i]= np.linspace(-lim,lim,N)
-    #     j = 0
-    #     for coor in zip(coorZip[0],coorZip[1]):
-    #         Z[i,j] = func(coor[0],coor[1])
-    #         j = j + 1
-    #     i = i + 1
-    for xx in X:
-        lim = func_amp(xx[i])
-        Y[:,i]= np.linspace(-lim,lim,N)
-        j = 0
-        for coor in zip(xx,Y[:,i]):
-            Z[i,j] = func(coor[0],coor[1])
-            j = j + 1
-        i = i + 1
-
-        
-# Compute data
-N = 200
-lim = R
-x = np.linspace(-L_AR,X_M+L_AV,N)
-y= np.linspace(-lim,lim,N)
-
-X, Y = np.meshgrid(x, y)
-Y_neg = -Y
-
-# AV
-Z_1 = np.zeros(X.shape)
-Z_2 = np.zeros(X.shape)
-Z_3 = np.zeros(X.shape)
-Z_4 = np.zeros(X.shape)
-
-compute_grid(X,Y,Z_1,f,f_amp)
-compute_grid(X,Y,Z_2,g,f_amp)
-# compute_grid(X,Y_neg,Z_3,f)
-# compute_grid(X,Y_neg,Z_4,g)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z');
+    plt.show();
 
 
+theta = np.linspace(0,2*np.pi,N)
+
+z = np.linspace(0,L_AV+X_M,N)
+[TH,Z] = np.meshgrid(theta,z)
+# r = f_av(Z)
+r = np.zeros(Z.shape)
+# r[Z<X_M] = f_m(Z[Z<X_M])
+r[(Z >= X_M) & (Z <= X_M + L_AV)] = f_av(Z[(Z >= X_M) & (Z <= X_M + L_AV)])
+
+
+# plotShape(TH_AV,r_av,Z_AV)
+
+# z_m = np.linspace(0,X_M,N)
+# [TH_M,Z_M] = np.meshgrid(theta,z_m)
+# r_m = f_m(Z_M)
+
+# plotShape(TH_M,r_m,Z_M)
+
+# Z = np.concatenate((Z_M,Z_AV), axis=0)
+# TH = np.concatenate((TH_M,TH_AV), axis=0)
+# r = np.concatenate((r_m,r_av), axis=0)
+
+
+[x,y,z] = pol2cart(TH,r,Z)
 
 #Plot 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 # plot AV
-ax.plot_surface(X, Y, Z_1, cmap='binary')
-ax.plot_surface(X, Y, Z_2, cmap='spring')
-ax.set_zlim(-lim, lim)
+ax.plot_surface(x,y,z, cmap='binary')
 
-# ax.contour3D(X, Y_neg, Z_3, 50, cmap='binary')
-# ax.contour3D(X, Y_neg, Z_4, 50, cmap='binary')
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z');
-plt.show()
+plt.show();
+print("debug")
